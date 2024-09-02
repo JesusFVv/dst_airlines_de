d4ed1335356638d33384f75b7dfd691ef6b3ea5c
Alexandre <alexandre.morvan@safrangroup.com>
2024-09-02 20:39:25 +0000

    Update code

--- bin/customer_flight_info/cooked_loading/load_customer_flight_info_cooked.py
+++ bin/customer_flight_info/cooked_loading/load_customer_flight_info_cooked.py
@@ -1,4 +1,5 @@
 import logging
+import psycopg2
 from common import utils
 from datetime import datetime, timedelta
 from pathlib import Path, PosixPath
@@ -8,13 +9,15 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def flatten_info(data_subset: dict[str, str], expected_keys: list[tuple[str, str]]) -> dict[str, str]:
+def flatten_info(
+    data_subset: dict[str, str], expected_keys: list[tuple[str, str]]
+) -> dict[str, str]:
     """Read information from a subset of raw data to make them suitable for the cooked table
 
     Args:
         data_subset (dict[str, str]): data coming from 'Terminal' or 'Status' or 'OperatingCarrier' or 'Equipment' columns in raw table
         expected_keys (list[tuple[str, str]]): each tuple is composed of a key to read in raw data and its associated key to write in cooked table
-    
+
     Returns:
         info (dict[str, str]): a dictionary returning formatted data according to cooked table schema
     """
@@ -23,11 +26,16 @@ def flatten_info(data_subset: dict[str, str], expected_keys: list[tuple[str, str
     for data_key, table_key in expected_keys:
         if data_key in keys:
             info[table_key] = data_subset[data_key]
-    
+
     return info
 
 
-def generate_datetime_info(data_subset: dict[str, str], airport_code: str, db_config_filepath: PosixPath, utc_offset_airport_query: str) -> str:
+def generate_datetime_info(
+    data_subset: dict[str, str],
+    airport_code: str,
+    db_config_filepath: PosixPath,
+    utc_offset_airport_query: str,
+) -> str:
     """Generate a datetime in proper format from a raw data subset having date and time information
 
     Args:
@@ -43,24 +51,37 @@ def generate_datetime_info(data_subset: dict[str, str], airport_code: str, db_co
     if "DateTime" in keys:
         date_time = data_subset["DateTime"]  # UTC time of the airport
     elif "Date" in keys and "Time" in keys:
-        date_time = f'{data_subset["Date"]}T{data_subset["Time"]}'  # Local time of the airport
+        date_time = (
+            f'{data_subset["Date"]}T{data_subset["Time"]}'  # Local time of the airport
+        )
         # Transform local time to UTC time of the airport
-        utc_offset_airport = utils.read_data_from_db(db_config_filepath, utc_offset_airport_query.format(airport_code))
+        utc_offset_airport = utils.read_data_from_db(
+            db_config_filepath, utc_offset_airport_query.format(airport_code)
+        )
         if utc_offset_airport:
             utc_offset = int(utc_offset_airport[0][0])
-            date_time = datetime.strptime(date_time, "%Y-%m-%dT%H:%M") + timedelta(hours=utc_offset)
+            date_time = datetime.strptime(date_time, "%Y-%m-%dT%H:%M") + timedelta(
+                hours=utc_offset
+            )
             date_time = date_time.strftime("%Y-%m-%dT%H:%M")
         else:
             pass
-            #TODO: what to do if the utc offset is not available for a given airprot 
+            # TODO: what to do if the utc offset is not available for a given airprot
 
     else:
-        date_time = f'{data_subset["Date"]}T{"00:00"}'  # Put default time value at 00:00
+        date_time = (
+            f'{data_subset["Date"]}T{"00:00"}'  # Put default time value at 00:00
+        )
 
     return date_time
 
 
-def process_data(data_subset: dict[str, str | dict], key_name: str, db_config_filepath: PosixPath, utc_offset_airport_query: str) -> dict[str, str]:
+def process_data(
+    data_subset: dict[str, str | dict],
+    key_name: str,
+    db_config_filepath: PosixPath,
+    utc_offset_airport_query: str,
+) -> dict[str, str]:
     """Process a subset of raw data to make them suitable for the cooked table
 
     Args:
@@ -76,41 +97,66 @@ def process_data(data_subset: dict[str, str | dict], key_name: str, db_config_fi
     key_name = key_name.strip().lower()
     keys = data_subset.keys()
     if "AirportCode" in keys:
-        res[f"{key_name}_airport_code"] = data_subset["AirportCode"].upper()
+        res[f"{key_name}_airport_code"] = data_subset["AirportCode"].strip().upper()
 
     if "Scheduled" in keys:
-        scheduled_date_time = generate_datetime_info(data_subset["Scheduled"], res[f"{key_name}_airport_code"], db_config_filepath, utc_offset_airport_query)
+        scheduled_date_time = generate_datetime_info(
+            data_subset["Scheduled"],
+            res[f"{key_name}_airport_code"],
+            db_config_filepath,
+            utc_offset_airport_query,
+        )
         res[f"{key_name}_scheduled_datetime"] = scheduled_date_time
 
     if "Estimated" in keys:
-        estimated_date_time = generate_datetime_info(data_subset["Estimated"], res[f"{key_name}_airport_code"], db_config_filepath, utc_offset_airport_query)
+        estimated_date_time = generate_datetime_info(
+            data_subset["Estimated"],
+            res[f"{key_name}_airport_code"],
+            db_config_filepath,
+            utc_offset_airport_query,
+        )
         res[f"{key_name}_estimated_datetime"] = estimated_date_time
-    
+
     if "Actual" in keys:
-        actual_date_time = generate_datetime_info(data_subset["Actual"], res[f"{key_name}_airport_code"], db_config_filepath, utc_offset_airport_query)
+        actual_date_time = generate_datetime_info(
+            data_subset["Actual"],
+            res[f"{key_name}_airport_code"],
+            db_config_filepath,
+            utc_offset_airport_query,
+        )
         res[f"{key_name}_actual_datetime"] = actual_date_time
 
     if "Terminal" in keys:
         # Create a list where each tuple is a pair between the raw data key and the table column name
-        terminal_expected_keys = [("Name", f"{key_name}_terminal_name"), ("Gate", f"{key_name}_terminal_gate")]
+        terminal_expected_keys = [
+            ("Name", f"{key_name}_terminal_name"),
+            ("Gate", f"{key_name}_terminal_gate"),
+        ]
         terminal_info = flatten_info(data_subset["Terminal"], terminal_expected_keys)
         # Make values as uppercase
-        terminal_info_upper = {k:v.upper() for (k,v) in terminal_info.items()}
+        terminal_info_upper = {k: v.strip().upper() for (k, v) in terminal_info.items()}
         res.update(terminal_info_upper)
-    
+
     if "Status" in keys:
         # Create a list where each tuple is a pair between the raw data key and the table column name
-        status_expected_keys = [("Code", f"{key_name}_status_code"), ("Description", f"{key_name}_status_description")]
+        status_expected_keys = [
+            ("Code", f"{key_name}_status_code"),
+            ("Description", f"{key_name}_status_description"),
+        ]
         status_info = flatten_info(data_subset["Status"], status_expected_keys)
         # Make a value as uppercase
         if f"{key_name}_status_code" in status_info:
-            status_info[f"{key_name}_status_code"] = status_info[f"{key_name}_status_code"].upper()
+            status_info[f"{key_name}_status_code"] = status_info[
+                f"{key_name}_status_code"
+            ].strip().upper()
         res.update(status_info)
 
     return res
 
 
-def _build_flat_data(data: dict, db_config_filepath: PosixPath, utc_offset_airport_query: str) -> dict[str, str] | None:
+def _build_flat_data(
+    data: dict, db_config_filepath: PosixPath, utc_offset_airport_query: str
+) -> dict[str, str] | None:
     """Transform each key/value pairs coming from raw table to make them suitable for the cooked table
 
     Args:
@@ -124,46 +170,66 @@ def _build_flat_data(data: dict, db_config_filepath: PosixPath, utc_offset_airpo
     formatted_dict = {}
     keys = data.keys()
     if "Departure" in keys:
-        departure_data = process_data(data["Departure"], "Departure", db_config_filepath, utc_offset_airport_query)
+        departure_data = process_data(
+            data["Departure"], "Departure", db_config_filepath, utc_offset_airport_query
+        )
         formatted_dict.update(departure_data)
-    else:  # If 'Departure' key is not in raw data, skip the file
+    else:  # If 'Departure' key is not in raw data, exit this function (skip the file)
         return
 
     if "Arrival" in keys:
-        arrival_data = process_data(data["Arrival"], "Arrival", db_config_filepath, utc_offset_airport_query)
+        arrival_data = process_data(
+            data["Arrival"], "Arrival", db_config_filepath, utc_offset_airport_query
+        )
         formatted_dict.update(arrival_data)
-    else:  # If 'Arrival' key is not in raw data, skip the file
+    else:  # If 'Arrival' key is not in raw data, exit this function (skip the file)
         return
-    
+
     if "OperatingCarrier" in keys:
         # Create a list where each tuple is a pair between the raw data key and the table column name
-        operating_carrier_expected_keys = [("AirlineID", "operating_airline_id"), ("FlightNumber", "operating_flight_nb")]
-        operating_carrier_info = flatten_info(data["OperatingCarrier"], operating_carrier_expected_keys)
+        operating_carrier_expected_keys = [
+            ("AirlineID", "operating_airline_id"),
+            ("FlightNumber", "operating_flight_nb"),
+        ]
+        operating_carrier_info = flatten_info(
+            data["OperatingCarrier"], operating_carrier_expected_keys
+        )
         # Make values as uppercase
-        operating_carrier_info_upper = {k:v.upper() for (k,v) in operating_carrier_info.items()}
+        operating_carrier_info_upper = {
+            k: v.strip().upper() for (k, v) in operating_carrier_info.items()
+        }
         formatted_dict.update(operating_carrier_info_upper)
-    
+
     if "Equipment" in keys:
         # Create a list where each tuple is a pair between the raw data key and the table column name
         equipment_expected_keys = [("AircraftCode", "equipment_aircraft_code")]
         equipment_info = flatten_info(data["Equipment"], equipment_expected_keys)
         # Make values as uppercase
-        equipment_info_upper = {k:v.upper() for (k,v) in equipment_info.items()}
+        equipment_info_upper = {k: v.strip().upper() for (k, v) in equipment_info.items()}
         formatted_dict.update(equipment_info_upper)
 
     if "Status" in keys:
         # Create a list where each tuple is a pair between the raw data key and the table column name
-        overall_status_expected_keys = [("Code", "overall_status_code"), ("Description", "overall_status_description")]
+        overall_status_expected_keys = [
+            ("Code", "overall_status_code"),
+            ("Description", "overall_status_description"),
+        ]
         overall_status_info = flatten_info(data["Status"], overall_status_expected_keys)
         # Make a value as uppercase
         if "overall_status_code" in overall_status_info:
-            overall_status_info["overall_status_code"] = overall_status_info["overall_status_code"].upper()
+            overall_status_info["overall_status_code"] = overall_status_info[
+                "overall_status_code"
+            ].strip().upper()
         formatted_dict.update(overall_status_info)
-    
+
     return formatted_dict
 
 
-def build_flat_data(all_data: list[tuple[int, dict]], db_config_filepath: PosixPath, utc_offset_airport_query: str) -> Generator[dict, None, None]:
+def build_flat_data(
+    all_data: list[tuple[int, dict]],
+    db_config_filepath: PosixPath,
+    utc_offset_airport_query: str,
+) -> Generator[dict, None, None]:
     """Format data coming from database raw table to make them suitable for the cooked table
 
     Args:
@@ -175,7 +241,9 @@ def build_flat_data(all_data: list[tuple[int, dict]], db_config_filepath: PosixP
         formatted_dict (Generator[dict, None, None]): a generator returning formatted data as a dictionary for each cooked table row
     """
     for _, data in all_data:
-        formatted_dict = _build_flat_data(data, db_config_filepath, utc_offset_airport_query)
+        formatted_dict = _build_flat_data(
+            data, db_config_filepath, utc_offset_airport_query
+        )
         if formatted_dict:
             yield formatted_dict
 
@@ -189,12 +257,25 @@ def ingest_data(
 
     Args:
         db_config_filepath (PosixPath): absolute path to the db config file
-        sql_table_name_cooked (str): SQL table name where cooked data are stored
+        sql_table_name_cooked (str): SQL table name where raw cooked are stored
         gen (Generator[dict, None, None]): a generator returning formatted data as a dictionary
     """
     # Database connection
     conn, cur = utils.connect_db(db_config_filepath)
 
+    try:
+        for cooked_data in gen:
+            utils.insert_data_into_db(cur, sql_table_name_cooked, cooked_data)
+            conn.commit()
+    except (Exception, psycopg2.Error) as e:
+        logger.exception(e)
+        conn.rollback()
+    finally:
+        # Close database connection
+        if conn:
+            cur.close()
+        conn.close()
+        logger.info("Connection closed to the database")
 
 
 if __name__ == "__main__":
@@ -204,18 +285,25 @@ if __name__ == "__main__":
     db_config_filepath = Path(
         "/home/ubuntu/dst_airlines_de/bin/customer_flight_info/cooked_loading/common/database.ini"
     )
-    raw_data_query = "SELECT * FROM operations_customer_flight_info_raw"
-    utc_offset_airport_query = "SELECT UTC_offset FROM refdata_airports_coo WHERE Airport={}"
+    raw_table_name = "operations_customer_flight_info_raw"
+    raw_data_query = f"SELECT * FROM {raw_table_name}"
+
+    cooked_airports_table_name = "refdata_airports_coo"
+    utc_offset_airport_query = f"SELECT utc_offset FROM {cooked_airports_table_name}"
+    utc_offset_airport_query += " WHERE airport={}"  # The WHERE clause will be filled later with airport code
+
+    sql_table_name_cooked = "operations_customer_flight_info_coo"
 
     ########################
     # Read data from database raw table
-    all_data = utils.read_data_from_db(db_config_filepath, raw_data_query)  # Returns a list of tuples. The tuple is made of 2 elements: id (int) and data (dict)
+    all_data = utils.read_data_from_db(
+        db_config_filepath, raw_data_query
+    )  # Returns a list of tuples. The tuple is made of 2 elements: id (int) and data (dict)
     # Build data structure for cooked table
-    gen = build_flat_data(all_data, db_config_filepath, utc_offset_airport_query)  # Generator object
+    gen = build_flat_data(
+        all_data, db_config_filepath, utc_offset_airport_query
+    )  # Generator object
+    # Ingest data into cooked table
+    ingest_data(db_config_filepath, sql_table_name_cooked, gen)
+    logger.info("COOKED LOADING COMPLETED !")
 
-
-# [
-#     (1, {'Status': {'Code': 'LD', 'Description': 'Flight Landed'}, 'Arrival': {'Actual': {'Date': '2024-07-06', 'Time': '07:52'}, 'Status': {'Code': 'LD', 'Description': 'Flight Landed'}, 'Scheduled': {'Date': '2024-07-06', 'Time': '07:50'}, 'AirportCode': 'ZAD'}, 'Departure': {'Actual': {'Date': '2024-07-06', 'Time': '06:17'}, 'Status': {'Code': 'DP', 'Description': 'Flight Departed'}, 'Terminal': {'Gate': 'B15', 'Name': '1'}, 'Scheduled': {'Date': '2024-07-06', 'Time': '06:10'}, 'AirportCode': 'BER'}, 'Equipment': {'AircraftCode': '320'}, 'OperatingCarrier': {'AirlineID': '4X', 'FlightNumber': '8964'}}),
-#     (2, {'Status': {'Code': 'LD', 'Description': 'Flight Landed'}, 'Arrival': {'Actual': {'Date': '2024-07-06', 'Time': '22:45'}, 'Status': {'Code': 'LD', 'Description': 'Flight Landed'}, 'Scheduled': {'Date': '2024-07-06', 'Time': '22:05'}, 'AirportCode': 'WAW'}, 'Departure': {'Actual': {'Date': '2024-07-06', 'Time': '21:16'}, 'Status': {'Code': 'DP', 'Description': 'Flight Departed'}, 'Terminal': {'Gate': 'A13', 'Name': '1'}, 'Scheduled': {'Date': '2024-07-06', 'Time': '20:30'}, 'AirportCode': 'FRA'}, 'Equipment': {'AircraftCode': '32A'}, 'OperatingCarrier': {'AirlineID': 'LH', 'FlightNumber': '1352'}, 'MarketingCarrierList': {'MarketingCarrier': [{'AirlineID': 'A3', 'FlightNumber': '1474'}, {'AirlineID': 'NH', 'FlightNumber': '5887'}]}}),
-#     (3, {'Status': {'Code': 'LD', 'Description': 'Flight Landed'}, 'Arrival': {'Actual': {'Date': '2024-07-06', 'Time': '23:32'}, 'Status': {'Code': 'LD', 'Description': 'Flight Landed'}, 'Scheduled': {'Date': '2024-07-06', 'Time': '22:05'}, 'AirportCode': 'WAW'}, 'Departure': {'Actual': {'Date': '2024-07-06', 'Time': '22:07'}, 'Status': {'Code': 'DP', 'Description': 'Flight Departed'}, 'Terminal': {'Gate': 'G43', 'Name': '2'}, 'Scheduled': {'Date': '2024-07-06', 'Time': '20:35'}, 'AirportCode': 'MUC'}, 'Equipment': {'AircraftCode': '31K'}, 'OperatingCarrier': {'AirlineID': 'CL', 'FlightNumber': '1616'}, 'MarketingCarrierList': {'MarketingCarrier': [{'AirlineID': 'A3', 'FlightNumber': '1507'}, {'AirlineID': 'LO', 'FlightNumber': '4908'}, {'AirlineID': 'UA', 'FlightNumber': '9495'}, {'AirlineID': 'WY', 'FlightNumber': '5291'}]}})
-# ]
