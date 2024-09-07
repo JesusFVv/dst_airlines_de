20bcd3a2c77e1d2e55dc3a7ca5d911af86bcade8
ChrisMSaf <christophe.martin3@safrangroup.com>
2024-09-07 21:30:50 +0200

    patch run docker

---
+++ bin/reference_data/raw_loading/run_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set some variables
+readonly container_name="python_load_raw_reference_data"
+readonly image_name_python="python:3.11-alpine3.20"
+readonly image_name_app="python_load_reference_data_raw:latest"
+readonly tmp_build_context="tmp_build_context"
+readonly network_name="dst_network"  # Coming from docker-compose file implementing the postgres db
+
+# Create a temporary folder that will serve as a build context for Docker
+if [ -d ${tmp_build_context} ]; then
+  rm -rf ${tmp_build_context}
+fi
+mkdir ${tmp_build_context}
+cp * ${tmp_build_context} &>/dev/null
+cp -r /home/ubuntu/dst_airlines_de/bin/common/ ${tmp_build_context}
+mv ${tmp_build_context}/common/.dockerignore ${tmp_build_context}
+cp -r /home/ubuntu/dst_airlines_de/src/project_deployment_postgres/*.txt ${tmp_build_context}
+cp -r /home/ubuntu/dst_airlines_de/src/project_deployment_postgres/*.yml ${tmp_build_context}
+mkdir ${tmp_build_context}/reference_data
+cp /home/ubuntu/dst_airlines_de/bin/reference_data/*.sh ${tmp_build_context}/reference_data
+cp /home/ubuntu/dst_airlines_de/bin/reference_data/*.py ${tmp_build_context}/reference_data
+cp /home/ubuntu/dst_airlines_de/bin/reference_data/*.sql ${tmp_build_context}/reference_data
+cp /home/ubuntu/dst_airlines_de/bin/reference_data/*.md ${tmp_build_context}/reference_data
+
+# Remove container - it might be there from a previous run
+docker container inspect ${container_name} &>/dev/null && docker container rm ${container_name}
+# Remove images - they might be there from a previous build
+docker image inspect ${image_name_python} &>/dev/null && docker image rm ${image_name_python}
+docker image inspect ${image_name_app} &>/dev/null && docker image rm ${image_name_app}
+# Build Docker image
+docker build -t ${image_name_app} ${tmp_build_context}
+# Remove temporary folder that served as a build context for Docker
+rm -rf ${tmp_build_context}
+# Run Docker
+docker run --volume /home/ubuntu/dst_airlines_de/data/customerFlightInfo:/home/ubuntu/dst_airlines_de/data/customerFlightInfo --network ${network_name} --name ${container_name} ${image_name_app}
