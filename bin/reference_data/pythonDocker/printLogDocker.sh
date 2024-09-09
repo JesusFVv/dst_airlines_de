ed6e30d091212d9d43d70107b1e65a68bd228e68
ChrisMSaf <christophe.martin3@safrangroup.com>
2024-09-09 10:11:55 +0200

    Cleaning: remove old and nonfunctional docker

--- bin/reference_data/pythonDocker/printLogDocker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# print log
-
-docker logs python_docker_reference_data
--- bin/reference_data/pythonDocker/runDocker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# run
-docker run -d \
-  --volume $(pwd)/python_reference_data:/app/output \
-  --name python_app_reference_data \
-  python_reference_data
-
