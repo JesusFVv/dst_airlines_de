5d6d306e5c3f4b7f88dc59379665c24bbf936ffd
Alexandre <alexandre.morvan@safrangroup.com>
2024-10-06 17:12:57 +0000

    Delete Raw data folders

--- bin/customer_flight_info/extraction/extract_customer_flight_info.py
+++ bin/customer_flight_info/extraction/extract_customer_flight_info.py
@@ -238,7 +238,7 @@ if __name__ == "__main__":
     )
     today = dt.date.today().strftime("%Y-%m-%d")
     # datetime_array = generate_datetime_array(yesterday_two_am, today)
-    datetime_array = generate_datetime_array("2024-09-13 02:00:00", "2024-09-14")
+    datetime_array = generate_datetime_array("2024-09-16 02:00:00", "2024-09-17")
     logger.debug(datetime_array)
 
     # Define retry strategy for https requests
