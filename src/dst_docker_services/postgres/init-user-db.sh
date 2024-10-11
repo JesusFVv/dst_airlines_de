d5b08f7da90c81144d42412202ada7b40c462ed6
Ubuntu <ubuntu@ip-172-31-2-159.eu-west-1.compute.internal>
2024-10-11 20:54:31 +0000

    dst services docker completed4

--- src/dst_docker_services/postgres/init-user-db.sh
+++ src/dst_docker_services/postgres/init-user-db.sh
@@ -15,3 +15,14 @@ EOSQL
 # GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
 # ALTER DEFAULT PRIVILEGES FOR ROLE $POSTGRES_USER IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO $POSTGRES_USER;
 # GRANT SELECT ON ALL TABLES IN SCHEMA public TO dst_reader GRANTED BY $POSTGRES_USER;
+
+# Create role and User for the PostgREST API
+# Create anonimous role to grant read acces to the specified schema and tables of the DB, for the web requests.
+# Create an user in order to login to the RESTAPI
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    create role web_anonimous nologin;
+    grant usage on schema public to web_anonimous;
+    grant select on all tables in schema public to web_anonimous;
+    create role postgrest_authenticator noinherit login password 'pass_api';
+    grant web_anonimous to postgrest_authenticator;
+EOSQL
