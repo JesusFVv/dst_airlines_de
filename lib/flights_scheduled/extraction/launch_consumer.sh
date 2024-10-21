9681b9db62ca368b8c012b2d73e6d7006e4e03a8
JesusFVv <fdz.verbo.j@gmail.com>
2024-10-21 13:29:41 +0200

    updated flights schedules dockerfile to integrate secrets.toml as a volume, to avoid having the secrets.toml into the image

--- lib/flights_scheduled/extraction/launch_consumer.sh
+++ lib/flights_scheduled/extraction/launch_consumer.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# Execute script with command:
+#   bash lib/flights_scheduled/extraction/launch_consumer.sh
 set -a
 source .env
 set +a
-CONTAINER_NAME=flight_schedules_consumer2
+CONTAINER_NAME=flight_schedules_consumer
 docker container stop ${CONTAINER_NAME} >& /dev/null
 docker container rm ${CONTAINER_NAME} >& /dev/null
 pushd lib/flights_scheduled/extraction
@@ -10,6 +12,9 @@ docker container run -d --name $CONTAINER_NAME \
     -e RABBITMQ_HOST=$RABBITMQ_HOST \
     -e RABBITMQ_PORT=$RABBITMQ_PORT \
     -e FLIGHT_SCHEDULES_CHANNEL=$FLIGHT_SCHEDULES_CHANNEL \
+    -e LOG_FILE_PATH=/usr/src/app/log \
+    -v ${FLIGHT_SCHEDULES_CONSUMER_DOCKER_PATH}/.dlt/secrets.toml:/usr/src/app/.dlt/secrets.toml:ro \
+    -v ${FLIGHT_SCHEDULES_CONSUMER_LOG_PATH}:/usr/src/app/log:rw \
     --network dst_network \
     --restart always \
     dst_flight_schedules_consumer
