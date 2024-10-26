503d3530ca7300b8d5f8bc5e7ef3e65bef19910d
JesusFVv <fdz.verbo.j@gmail.com>
2024-10-27 00:01:09 +0200

    DB dump with also CFI L2

--- lib/db_dump/dst_airlines_db_dump.sh
+++ lib/db_dump/dst_airlines_db_dump.sh
@@ -11,5 +11,5 @@ FILE_NAME=$(date +%Y%m%d_%H%M%S_)dst_airlines_db_dump.sql.gz
 docker exec $POSTGRES_CONTAINER_NAME sh -c "pg_dump $POSTGRES_DB -U dst_designer | gzip > /var/backups/$FILE_NAME"
 
 # Extract the SQL file from the GZIP
-# FILE_NAME=20241026_213116_dst_airlines_db_dump.sql.gz
+# FILE_NAME=20241026_235843_dst_airlines_db_dump.sql.gz
 # gunzip -c var/db_dumps/${FILE_NAME} > var/db_dumps/${FILE_NAME%.gz}
