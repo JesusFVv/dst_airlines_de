f91810b5d31ab988b5801cc4506a9947832f1c7b
JesusFVv <fdz.verbo.j@gmail.com>
2024-10-28 17:20:48 +0100

    Metabase.db commited wiht dashboard shown

---
+++ etc/metabase.md
@@ -0,0 +1,6 @@
+# Credentials
+
+- Jesus email/pass: jfv@sae.com / D?.boiLW$7/0,0L~Y=
+- Christophe email/pass: cm@sae.com / VhZptoTj0Uu977
+- Alexandre email/pass: am@sae.com / 787CuJOE1dFDRA
+- Reader: read@dst.com / McF1oGbeFmAV59
--- src/docker_services/metabase/launch_metabase_container.sh
+++ src/docker_services/metabase/launch_metabase_container.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# execute it with command: bash src/docker/nginx/launch_nginx_container.sh
+# execute it with command: bash src/docker_services/metabase/launch_metabase_container.sh
 
 set -a
 source .env
 set +a
 
-CONTAINER_NAME=metabase
+CONTAINER_NAME=metabase_bis
 docker container stop ${CONTAINER_NAME} >& /dev/null
 docker container rm ${CONTAINER_NAME} >& /dev/null
 docker run --name ${CONTAINER_NAME} \
@@ -13,6 +13,7 @@ docker run --name ${CONTAINER_NAME} \
 -e MB_DB_FILE=/metabase-data/metabase.db -e MUID=$SERVICES_UID -e MGID=$SERVICES_GID \
 -e "JAVA_TIMEZONE=Europe/Paris" -e "JAVA_OPTS=-Xmx3g" \
 -e MB_JETTY_HOST=0.0.0.0 -e MB_JETTY_PORT=3000 \
+-p 3000:3000 \
 --network dst_network \
 --restart unless-stopped \
 -d metabase/metabase
