7f6abeb70644d26acb3f9c4aaf68629f812716b6
JesusFVv <fdz.verbo.j@gmail.com>
2024-10-28 07:09:46 +0100

    dag CFI updated to stop using extra credentials for FS during 2h

--- var/airflow/dags/dag_customers_flight_info.py
+++ var/airflow/dags/dag_customers_flight_info.py
@@ -12,6 +12,25 @@ def customer_flight_info_ssh_operator():
         ssh_conn_id='WSL_Home',
         command=f"pushd {PROJECT_ABSOLUT_PATH} && bash lib/customer_flight_info/producer/launch_producer.sh "
     )
-    customer_flight_info_producer
+    
+    flight_schedules_consumer_partial_stop = SSHOperator(
+        task_id="flight_schedules_consumer_partial_stop",
+        ssh_conn_id='WSL_Home',
+        command=f"docker container stop dst_docker_services-flight_schedules_consumer_b-1 && docker container stop dst_docker_services-flight_schedules_consumer_b-2 "
+    )
+    
+    wait_until_end_of_customer_flight_information_consumer = SSHOperator(
+        task_id="wait_until_end_of_customer_flight_information_consumer",
+        ssh_conn_id='WSL_Home',
+        command=f"sleep 120m"
+    )
+    
+    flight_schedules_consumer_partial_start = SSHOperator(
+        task_id="flight_schedules_consumer_partial_start",
+        ssh_conn_id='WSL_Home',
+        command=f"docker container start dst_docker_services-flight_schedules_consumer_b-1 && docker container start dst_docker_services-flight_schedules_consumer_b-2 "
+    )
+        
+    flight_schedules_consumer_partial_stop >> customer_flight_info_producer >> wait_until_end_of_customer_flight_information_consumer >> flight_schedules_consumer_partial_start
 
 customer_flight_info_ssh_operator()
--- var/airflow/dags/dag_customers_flight_info_l2_incremental_loading.py
+++ var/airflow/dags/dag_customers_flight_info_l2_incremental_loading.py
@@ -8,10 +8,11 @@ PROJECT_ABSOLUT_PATH = os.environ['PROJECT_ABSOLUT_PATH']  # Import the env vari
 @dag(start_date=datetime(2024, 10, 20, tz="UTC"), schedule="0 3-6 * * *", catchup=False, tags=["dst_project", "customer_flight_info"])
 def customer_flight_info_l2_incremental_loading_ssh_operator():
     customer_flight_info_l2_incremental_loading = SSHOperator(
-        task_id="customer_flight_info_producer",
+        task_id="customer_flight_info_l2_incremental_loading",
         ssh_conn_id='WSL_Home',
         command=f"pushd {PROJECT_ABSOLUT_PATH} && bash lib/customer_flight_info/l2_loading_incremental/launch_docker_container.sh "
     )
+    
     customer_flight_info_l2_incremental_loading
 
 customer_flight_info_l2_incremental_loading_ssh_operator()
